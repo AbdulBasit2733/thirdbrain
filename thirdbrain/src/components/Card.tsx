b7621a54d1d9a570c80d3218023f056e1164538e
Abdul Basit <abdulbasitkhan2733@gmail.com>
2024-11-30 14:41:40 +0530

    Created Components

---
+++ thirdbrain/src/components/Card.tsx
@@ -0,0 +1,50 @@
+
+import ShareIcon from "../Icons/ShareIcon";
+import DeleteIcon from "../Icons/DeleteIcon";
+
+interface CardProps {
+  title: String;
+  link: String;
+  type: "twitter" | "youtube";
+}
+
+const Card = ({ title, link, type }: CardProps) => {
+  return (
+    <div>
+      <div className="shadow bg-white rounded-md border-[1.5px] border-gray-300  p-4 max-w-72 min-h-48 min-w-72">
+        <div className=" flex items-center justify-between">
+          <div className="flex items-center gap-5 text-gray-500">
+            <ShareIcon />
+            <h1 className="text-black font-semibold text-sm">{title}</h1>
+          </div>
+          <div className="flex gap-2 items-center text-gray-500">
+            <a href={link.toString()} target="_blank" className="cursor-pointer">
+              <ShareIcon />
+            </a>
+            <DeleteIcon />
+          </div>
+        </div>
+        <div className=" pt-5">
+          {type === "youtube" && (
+            <iframe
+              className=" w-full"
+              src={link.replace("watch", "embed")}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+          )}
+          {type === "twitter" && (
+            <blockquote className="twitter-tweet">
+              <a href={link}></a>
+            </blockquote>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- thirdbrain/tailwind.config.js
+++ thirdbrain/tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        purple:{
+          200:"#d9ddee",
+          500:'#9492db',
+          600:'#7164c0',
+
+        },
+        gary:{
+          100:'#eeeeef',
+          200:'#e6e9ed',
+          600:'#95989c'
+        }
+      }
+    },
   },
   plugins: [],
 };
