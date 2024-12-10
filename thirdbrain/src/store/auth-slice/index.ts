f5c91db6311282a3b4dc2953c67b468cbf72cb31
Abdul Basit <abdulbasitkhan2733@gmail.com>
2024-12-10 20:12:57 +0530

    Cors Errors

--- thirdbrain/src/store/auth-slice/index.ts
+++ thirdbrain/src/store/auth-slice/index.ts
@@ -1,11 +1,17 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import axios from "axios";
-import { InitialStateProps, User, AuthResponse, LoginUserArgs, RegisterUserArgs } from "../../types/types"; // Adjust the path as needed
+import {
+  InitialStateProps,
+  User,
+  AuthResponse,
+  LoginUserArgs,
+  RegisterUserArgs,
+} from "../../types/types"; // Adjust the path as needed
 
 const initialState: InitialStateProps = {
   isLoading: false,
   user: null,
-  contents:[],
+  contents: [],
   isAuthenticated: false,
 };
 
@@ -14,7 +20,9 @@ export const RegisterUser = createAsyncThunk(
   "/auth/register",
   async (formData: RegisterUserArgs) => {
     const response = await axios.post<AuthResponse>(
-      `${import.meta.env.VITE_BACKEND_BASEURL || "http://localhost:3000"}/api/v1/users/register`,
+      `${
+        import.meta.env.VITE_BACKEND_BASEURL || "http://localhost:3000"
+      }/api/v1/users/register`,
       formData,
       {
         withCredentials: true,
@@ -29,16 +37,18 @@ export const LoginUser = createAsyncThunk(
   "/auth/login",
   async (formData: LoginUserArgs) => {
     console.log(formData);
-    
+
     const response = await axios.post<AuthResponse>(
-      `${import.meta.env.VITE_BACKEND_BASEURL || "http://localhost:3000"}/api/v1/users/login`,
+      `${
+        import.meta.env.VITE_BACKEND_BASEURL || "http://localhost:3000"
+      }/api/v1/users/login`,
       formData,
       {
         withCredentials: true,
       }
     );
     console.log(response);
-    
+
     return response.data;
   }
 );
@@ -48,7 +58,9 @@ export const LogoutUser = createAsyncThunk<AuthResponse>(
   "/auth/logout",
   async () => {
     const response = await axios.post<AuthResponse>(
-      `${import.meta.env.VITE_BACKEND_BASEURL || "http://localhost:3000"}/api/v1/users/logout`,
+      `${
+        import.meta.env.VITE_BACKEND_BASEURL || "http://localhost:3000"
+      }/api/v1/users/logout`,
       {},
       {
         withCredentials: true,
@@ -63,11 +75,14 @@ export const CheckAuthentication = createAsyncThunk(
   "/auth/check-auth",
   async () => {
     const response = await axios.get<AuthResponse>(
-      `${import.meta.env.VITE_BACKEND_BASEURL || "http://localhost:3000"}/api/v1/users/check-auth`,
+      `${
+        import.meta.env.VITE_BACKEND_BASEURL || "http://localhost:3000"
+      }/api/v1/users/check-auth`,
       {
         withCredentials: true,
         headers: {
-          "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+          "Cache-Control":
+            "no-store, no-cache, must-revalidate, proxy-revalidate",
         },
       }
     );
@@ -89,22 +104,28 @@ const authSlice = createSlice({
       .addCase(RegisterUser.pending, (state) => {
         state.isLoading = true;
       })
-      .addCase(RegisterUser.fulfilled, (state, action: PayloadAction<AuthResponse>) => {
-        state.isLoading = false;
-        state.user = action.payload.user || null;
-        state.isAuthenticated = action.payload.success;
-      })
+      .addCase(
+        RegisterUser.fulfilled,
+        (state, action: PayloadAction<AuthResponse>) => {
+          state.isLoading = false;
+          state.user = action.payload.user || null;
+          state.isAuthenticated = action.payload.success;
+        }
+      )
       .addCase(RegisterUser.rejected, (state) => {
         state.isLoading = false;
       })
       .addCase(LoginUser.pending, (state) => {
         state.isLoading = true;
       })
-      .addCase(LoginUser.fulfilled, (state, action: PayloadAction<AuthResponse>) => {
-        state.isLoading = false;
-        state.user = action.payload.user || null;
-        state.isAuthenticated = action.payload.success;
-      })
+      .addCase(
+        LoginUser.fulfilled,
+        (state, action: PayloadAction<AuthResponse>) => {
+          state.isLoading = false;
+          state.user = action.payload.user || null;
+          state.isAuthenticated = action.payload.success;
+        }
+      )
       .addCase(LoginUser.rejected, (state) => {
         state.isLoading = false;
         state.user = null;
@@ -113,11 +134,14 @@ const authSlice = createSlice({
       .addCase(CheckAuthentication.pending, (state) => {
         state.isLoading = true;
       })
-      .addCase(CheckAuthentication.fulfilled, (state, action: PayloadAction<AuthResponse>) => {
-        state.isLoading = false;
-        state.user = action.payload.user || null;
-        state.isAuthenticated = action.payload.success;
-      })
+      .addCase(
+        CheckAuthentication.fulfilled,
+        (state, action: PayloadAction<AuthResponse>) => {
+          state.isLoading = false;
+          state.user = action.payload.user || null;
+          state.isAuthenticated = action.payload.success;
+        }
+      )
       .addCase(CheckAuthentication.rejected, (state) => {
         state.isLoading = false;
         state.user = null;
