e5fa81a0903a2ce0d0deaa159ca19227751a8b8c
Abdul Basit <abdulbasitkhan2733@gmail.com>
2024-12-19 22:16:02 +0530

    Changes Some things for deployement

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -1,2 +1,2 @@
-export const VITE_BACKEND_URL = "http://localhost:8000";
+export const VITE_BACKEND_URL = "https://thirdbrain-backend.vercel.app";
 export const VITE_FRONTEND_URL = "https://thirdbrain-kohl.vercel.app";
